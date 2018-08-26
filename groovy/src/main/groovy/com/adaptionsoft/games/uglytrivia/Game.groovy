dc29b543d0cd02db9ba86da73794bc5855f22be9
Alexandru Bolboaca <alexboly@gmail.com>
2018-08-26 11:08:03 +0300

    Cosmetic fix. Ignoring stuff

--- groovy/src/main/groovy/com/adaptionsoft/games/uglytrivia/Game.groovy
+++ groovy/src/main/groovy/com/adaptionsoft/games/uglytrivia/Game.groovy
@@ -1,83 +1,81 @@
 package com.adaptionsoft.games.uglytrivia
 
-public class Game {
-    ArrayList players = new ArrayList()
-    int[] places = new int[6]
-    int[] purses  = new int[6]
-    boolean[] inPenaltyBox  = new boolean[6]
-    
-    LinkedList popQuestions = new LinkedList()
-    LinkedList scienceQuestions = new LinkedList()
-    LinkedList sportsQuestions = new LinkedList()
-    LinkedList rockQuestions = new LinkedList()
-    
-    int currentPlayer = 0
-    boolean isGettingOutOfPenaltyBox
-    
-    public  Game(){
-    	50.times { def i ->
+class Game {
+	ArrayList players = new ArrayList()
+	int[] places = new int[6]
+	int[] purses = new int[6]
+	boolean[] inPenaltyBox = new boolean[6]
+
+	LinkedList popQuestions = new LinkedList()
+	LinkedList scienceQuestions = new LinkedList()
+	LinkedList sportsQuestions = new LinkedList()
+	LinkedList rockQuestions = new LinkedList()
+
+	int currentPlayer = 0
+	boolean isGettingOutOfPenaltyBox
+
+	Game() {
+		50.times { def i ->
 			popQuestions.addLast("Pop Question " + i)
 			scienceQuestions.addLast(("Science Question " + i))
 			sportsQuestions.addLast(("Sports Question " + i))
 			rockQuestions.addLast(createRockQuestion(i))
-    	}
-    }
+		}
+	}
 
-	public String createRockQuestion(int index){
+	String createRockQuestion(int index) {
 		return "Rock Question " + index
 	}
-	
-	public boolean isPlayable() {
+
+	boolean isPlayable() {
 		return (howManyPlayers() >= 2)
 	}
 
-	public boolean add(String playerName) {
-		
-		
-	    players.add(playerName)
-	    places[howManyPlayers()] = 0
-	    purses[howManyPlayers()] = 0
-	    inPenaltyBox[howManyPlayers()] = false
-	    
-	    println playerName + " was added"
-	    println "They are player number " + players.size()
+	boolean add(String playerName) {
+		players.add(playerName)
+		places[howManyPlayers()] = 0
+		purses[howManyPlayers()] = 0
+		inPenaltyBox[howManyPlayers()] = false
+
+		println playerName + " was added"
+		println "They are player number " + players.size()
 		return true
 	}
-	
-	public int howManyPlayers() {
+
+	int howManyPlayers() {
 		return players.size()
 	}
 
-	public void roll(int roll) {
+	void roll(int roll) {
 		println players.get(currentPlayer) + " is the current player"
 		println "They have rolled a " + roll
-		
+
 		if (inPenaltyBox[currentPlayer]) {
 			if (roll % 2 != 0) {
 				isGettingOutOfPenaltyBox = true
-				
+
 				println players.get(currentPlayer) + " is getting out of the penalty box"
 				places[currentPlayer] = places[currentPlayer] + roll
 				if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12
-				
-    			println "${players.get(currentPlayer)}'s new location is ${places[currentPlayer]}"
+
+				println "${players.get(currentPlayer)}'s new location is ${places[currentPlayer]}"
 				println "The category is " + currentCategory()
 				askQuestion()
 			} else {
 				println players.get(currentPlayer) + " is not getting out of the penalty box"
 				isGettingOutOfPenaltyBox = false
-				}
-			
+			}
+
 		} else {
-		
+
 			places[currentPlayer] = places[currentPlayer] + roll
 			if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12
-			
+
 			println "${players.get(currentPlayer)}'s new location is ${places[currentPlayer]}"
 			println "The category is " + currentCategory()
 			askQuestion()
 		}
-		
+
 	}
 
 	private void askQuestion() {
@@ -88,10 +86,10 @@ public class Game {
 		if (currentCategory() == "Sports")
 			println sportsQuestions.removeFirst()
 		if (currentCategory() == "Rock")
-			println rockQuestions.removeFirst()		
+			println rockQuestions.removeFirst()
 	}
-	
-	
+
+
 	private String currentCategory() {
 		if (places[currentPlayer] == 0) return "Pop"
 		if (places[currentPlayer] == 4) return "Pop"
@@ -105,45 +103,44 @@ public class Game {
 		return "Rock"
 	}
 
-	public boolean wasCorrectlyAnswered() {
-		if (inPenaltyBox[currentPlayer]){
+	boolean wasCorrectlyAnswered() {
+		if (inPenaltyBox[currentPlayer]) {
 			if (isGettingOutOfPenaltyBox) {
 				println "Answer was correct!!!!"
 				purses[currentPlayer]++
-    			println "${players.get(currentPlayer)} now has ${purses[currentPlayer]} Gold Coins."
-				
+				println "${players.get(currentPlayer)} now has ${purses[currentPlayer]} Gold Coins."
+
 				boolean winner = didPlayerWin()
 				currentPlayer++
 				if (currentPlayer == players.size()) currentPlayer = 0
-				
+
 				return winner
 			} else {
 				currentPlayer++
 				if (currentPlayer == players.size()) currentPlayer = 0
 				return true
 			}
-			
-			
-			
+
+
 		} else {
-		
+
 			println "Answer was corrent!!!!"
 			purses[currentPlayer]++
 			println "${players.get(currentPlayer)} now has ${purses[currentPlayer]} Gold Coins."
-			
+
 			boolean winner = didPlayerWin()
 			currentPlayer++
 			if (currentPlayer == players.size()) currentPlayer = 0
-			
+
 			return winner
 		}
 	}
-	
-	public boolean wrongAnswer(){
+
+	boolean wrongAnswer() {
 		println "Question was incorrectly answered"
-		println players.get(currentPlayer)+ " was sent to the penalty box"
+		println players.get(currentPlayer) + " was sent to the penalty box"
 		inPenaltyBox[currentPlayer] = true
-		
+
 		currentPlayer++
 		if (currentPlayer == players.size()) currentPlayer = 0
 		return true
